033443b45430d56371b6ba5f831adc5321bf544f
JustPav <aksenovpaveldmitrievich@gmail.com>
2026-01-13 12:11:48 +0400

    Первый коммит с одной лабораторной

---
+++ Lab7/Lab7VarA.c
@@ -0,0 +1,64 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void AddElement(int **array, int *capacity, int ind, int value);
+void InputArray(int **array, int size);
+void UpdateArray(int **array, int size);
+void OutputArray(int *array, int size);
+
+int main() {
+    int *array;
+    int size = 0;
+
+    printf("Введите колличество элементов: ");
+
+    while (!(scanf("%d", &size) == 1 && getchar() == '\n')) {
+        printf("Ошибка! Не число! Попробуйте ещё раз: ");
+        while (getchar() != '\n');
+    }
+
+    InputArray(&array, size);
+
+    UpdateArray(&array, size);
+
+    OutputArray(array, size);
+}
+
+void AddElement(int **array, int *capacity, int ind, int value) {
+    if (ind >= *capacity) {
+        *capacity = (*capacity == 0) ? 1 : (*capacity) * 2;
+        *array = (int*)realloc(*array, (*capacity) * sizeof(int));
+    }
+    (*array)[ind] = value;
+}
+
+void InputArray(int **array, int size) {
+    int element;
+    int capacity = 0;
+
+    *array = (int*)calloc(size, sizeof(int));
+
+    for (int ind = 0; ind < size; ind++) {
+        printf("Введите элемент с новой строки: ");
+
+        while (!(scanf("%d", &element) == 1 && getchar() == '\n')) {
+            printf("Ошибка! Не число! Попробуйте ещё раз: ");
+            while (getchar() != '\n');
+        }
+        AddElement(array, &capacity, ind, element);
+    }
+}
+
+void OutputArray(int *array, int size) {
+    printf("Введённые элементы массива: ");
+
+    for (int ind = 0; ind < size; ind++) {
+        printf("%d ", array[ind]);
+    }
+}
+
+void UpdateArray(int **array, int size) {
+    for (int ind = 0; ind < size; ind++) {
+        if ((*array)[ind] % 2 == 0) (*array)[ind] = (*array)[ind] * -1;
+    }
+}
